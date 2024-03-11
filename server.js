c760e9961aba1caf6ac2e83cc5b3e30105ea943f
Meghana <meghanamanchal@gmail.com>
2024-03-11 18:29:44 +0530

    added changes

--- server.js
+++ server.js
@@ -8,6 +8,20 @@ app.get('/ping',(req,res) =>{
     console.error('Error handling request:', error);
   }
 })
+app.get('/onemorepls',(req,res) =>{
+  try{
+    return res.send('onece moreee!!!')
+  }catch (error) {
+    console.error('Error handling request:', error);
+  }
+})
+app.get('/opp',(req,res) =>{
+  try{
+    return res.send('onece moreee!!!')
+  }catch (error) {
+    console.error('Error handling request:', error);
+  }
+})
 
 app.listen(3000,() =>{
     console.log(`server running on port 3000`)
