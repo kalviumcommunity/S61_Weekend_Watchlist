5aa8f180f1598ef2ec1702cbefb1feafb9aa8735
Meghana <meghanamanchal@gmail.com>
2024-03-20 14:26:01 +0530

    Added props

--- FrontEnd/client/src/App.jsx
+++ FrontEnd/client/src/App.jsx
@@ -3,7 +3,7 @@
 import './App.css';
 import Home from './Components/Home';
 import Movie from './Components/Movie';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route,Link } from 'react-router-dom';
 
 const dummyData = {
   title: "Inception",
@@ -14,10 +14,18 @@ const dummyData = {
 
 function App() {
   return (
+    <>
+    <nav className='navigation-bar'>
+      <ul className='nav-options'>
+     <li><a><Link to='/'>HOME</Link></a></li> 
+     <li> <a><Link to='/movie'>MOVIES WATCHLIST</Link></a></li>
+      </ul>
+    </nav>
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/movie" element={<Movie {...dummyData} />} /> 
     </Routes>
+    </>
   );
 }
 
--- FrontEnd/client/src/Components/Home.jsx
+++ FrontEnd/client/src/Components/Home.jsx
@@ -5,7 +5,6 @@ export default function Home() {
   return (
     <>
     <h1>Welcome to Weekend Watchlist</h1>
-  
     </>
   )
 }
