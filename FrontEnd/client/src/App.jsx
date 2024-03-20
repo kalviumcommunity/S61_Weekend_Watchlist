2dd44a20ecb65dd19141c8eb420017830ab0fa52
Meghana <meghanamanchal@gmail.com>
2024-03-20 10:22:21 +0530

    Added Task component with dummy data

--- FrontEnd/client/src/App.jsx
+++ FrontEnd/client/src/App.jsx
@@ -1,15 +1,24 @@
-import './App.css'
-import Home from './Components/Home';
-import {Routes,Route} from 'react-router-dom'
-function App() {
+// App.js
 
+import './App.css';
+import Home from './Components/Home';
+import Movie from './Components/Movie';
+import { Routes, Route } from 'react-router-dom';
+
+const dummyData = {
+  title: "Inception",
+  description: "A mind-bending sci-fi thriller directed by Christopher Nolan.",
+  priority: "High"
+};
+
+
+function App() {
   return (
-    <>
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/movie" element={<Movie {...dummyData} />} /> 
     </Routes>
-    </>
-  )
+  );
 }
 
 export default App;
